cacf25222dfcf6f1cae3e75c4017d39bbedd6418
Kim <kwu1@ualberta.ca>
2014-03-23 03:00:59 -0600

    attempting crawl spider

---
+++ BaseSpider/Matscrape/spiders/firstSpider.py
@@ -0,0 +1,23 @@
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.selector import Selector
+from Matscrape.items import DrugInfo
+
+class MatadorSpider(Spider):
+    name = "matador"
+    allowed_domains = ["http://matador.embl.de/drugs/"]
+    start_urls = [
+        "http://matador.embl.de/drugs/"
+    ]
+    
+
+    def parse(self, response):
+        sel = Selector(response)
+        sites = sel.xpath('//ul/li')
+        items=[]
+        for site in sites:
+            item=DrugInfo()
+            item['drugname'] =site.xpath('a/text()').extract()
+            item['link'] = site.xpath('a/@href').extract() 
+            return items
+
+
