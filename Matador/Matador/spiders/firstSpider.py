aaf0e9a477bea6f57df64393a4f069629c7f21a8
Kim <kwu1@ualberta.ca>
2014-03-26 11:54:22 -0600

    reverted to base spider, fixed links

---
+++ Matador/Matador/spiders/firstSpider.py
@@ -0,0 +1,21 @@
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from Matador.items import DrugInfo
+
+
+class MatadorSpider(Spider):
+    name = "matador"
+    allowed_domains = ["http://matador.embl.de/drugs/"]
+    start_urls = [
+        "http://matador.embl.de/drugs/"
+    ]
+
+    def parse(self, response):
+        sel = Selector(response)
+        sites = sel.xpath('//ul/li')
+        items=[]
+        for site in sites:
+            item=DrugInfo()
+            item['link'] = site.xpath('a/@href').extract()
+            items.append(item)
+        return items
